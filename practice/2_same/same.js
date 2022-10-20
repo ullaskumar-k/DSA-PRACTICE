67e49a807aa0407f05b02e284e62dde7e4507e91
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-20 09:02:16 +0530

    refactor code in same.js

--- practice/2_same/same.js
+++ practice/2_same/same.js
@@ -7,23 +7,39 @@ the array has its corresponding value squared in the second array. The frequency
 // same([1,2,3],[1,9]) //false
 // same([1,2,1],[4,4,1]) //false (must be same frequency) 
 
+//1st try
+// function same(a,b){
+//     if(a.length != b.length){
+//         return false;
+//     }
+//     var count = 0;
+//     for(let i=0;i<a.length;i++){
+//         for(let j=0;j<b.length;j++){
+//             if(a[i]*a[i] == b[j])
+//                 count++;                  
+//         }
+//     }
+//     if (count == a.length)
+//         return true;
+//     else
+//         return false;
+// }
+
 function same(a,b){
     if(a.length != b.length){
         return false;
     }
     var count = 0;
     for(let i=0;i<a.length;i++){
-        for(let j=0;j<b.length;j++){
-            if(a[i]*a[i] == b[j])
-                count++;                  
+        let d = b.indexOf(a[i] ** 2);
+        if(d === -1){
+            return false;                  
         }
+        b.splice(d,1);
     }
-    if (count == a.length)
-        return true;
-    else
-        return false;
+    return true;
 }
 
-v = same([1,2,1],[4,4,1]);
+v = same([1,2,3],[4,1,9]);
 console.log(v);
 
