2359fc45141b6f512b724ec9bfc3235940754bef
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-20 12:06:52 +0530

    Completed anagrams.js

--- practice/3_anagrams/anagrams.js
+++ practice/3_anagrams/anagrams.js
@@ -8,3 +8,29 @@
 // validAnagram("awesome","awesom") // false
 // validAnagram("qwerty","qeywrt") // true 
 // validAnagram("texttwisttime","timetwisttext") // true
+
+function validAnagram(a,b){
+    if(a.length != b.length){
+        return false;
+    }
+    let fc1 = {};
+    let fc2 = {};
+    for(let i in a){
+        fc1[a[i]] = (fc1[a[i]] || 0) + 1;
+    }
+    for(let i in b){
+        fc2[b[i]] = (fc2[b[i]] || 0) + 1;
+    }
+    for(let key in fc1){
+        if(!(key in fc2)){
+            return false;
+        }
+        if(!(fc1[key] == fc2[key])){
+            return false;
+        }
+    }
+    return true;
+}
+
+v = validAnagram("texttwisttime","timetwisttext")
+console.log(v)
