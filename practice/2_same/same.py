fad1a15e3912de0c9b57859e054488b5fec93349
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-20 11:04:35 +0530

    Refactore with frequency counter method in same.py file

--- practice/2_same/same.py
+++ practice/2_same/same.py
@@ -5,7 +5,8 @@
 #  same([1,2,3],[1,9]) //false
 #  same([1,2,1],[4,4,1]) //false (must be same frequency) 
 
-import timeit as t
+# import timeit as t
+
 # 1st try
 # def same(a,b):
 #     if len(a) != len(b):
@@ -19,23 +20,51 @@ import timeit as t
 #         return True
 #     return False
 
+# 2nd try
+# def same(a,b):
+#     if len(a) != len(b):
+#         return False    
+#     for i in a:
+#         try:
+#             d = b.index(i**2)
+#         except:
+#             return False    
+#         b.pop(d)
+#     return True    
 
+# a = t.timeit()
+
+# v = same([1,2,3],[4,1,9])
+# print(v)                   
+    
+# b = t.timeit()
+# time = b-a
+# print(f"Time Elapsed is {time} seconds")
+
+
+# frequency counter method
 def same(a,b):
     if len(a) != len(b):
-        return False    
-    for i in a:
+        return False
+    fc1 = {}
+    fc2 = {}
+    for key in a:
         try:
-            d = b.index(i**2)
-        except:
-            return False    
-        b.pop(d)
-    return True    
+            fc1[key] = (fc1[key] or 0) + 1
+        except KeyError:
+            pass     
+    for key in b:
+        try:
+            fc2[key] = (fc2[key] or 0) + 1
+        except KeyError:
+            pass     
+    for key in fc1:
+        if(not(key**2 in fc2)):
+            return False
+        if(not(fc1[key] == fc2[key**2])):
+            return False
+    return True
 
-a = t.timeit()
 
 v = same([1,2,3],[4,1,9])
 print(v)                   
-    
-b = t.timeit()
-time = b-a
-print(f"Time Elapsed is {time} seconds")
