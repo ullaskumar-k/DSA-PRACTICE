43264b14422943f3e257d9741a1839b3e17fbe5b
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-20 11:10:31 +0530

    add readme file

--- practice/2_same/same.js
+++ practice/2_same/same.js
@@ -1,12 +1,3 @@
-/*
-Write a function called same, which accepts two arrays. The function should return true if every value in 
-the array has its corresponding value squared in the second array. The frequency of values must be the same.
-*/
-
-// same([1,2,3],[4,1,9]) //true
-// same([1,2,3],[1,9]) //false
-// same([1,2,1],[4,4,1]) //false (must be same frequency) 
-
 //1st try
 // function same(a,b){
 //     if(a.length != b.length){
