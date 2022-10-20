2528818437f0fdc3b219f777c10ba1d78424d24c
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-20 13:01:19 +0530

    almost completed ana.py

--- practice/3_anagrams/anagrams.py
+++ practice/3_anagrams/anagrams.py
@@ -8,3 +8,43 @@
 # validAnagram("awesome","awesom") // false
 # validAnagram("qwerty","qeywrt") // true 
 # validAnagram("texttwisttime","timetwisttext") // true
+
+def validAnagram(a,b):
+    if len(a) != len(b):
+        return False
+    fc1 = {}
+    fc2 = {}
+    try:
+        for i in a:
+            fc1.update({i:(fc1.get(i) or 0)+1})
+    except:
+        pass
+    try:
+        for i in b:
+            fc2.update({i:(fc2.get(i) or 0)+1})
+    except:
+        pass
+    k = list(fc1.keys())
+    l = list(fc2.keys())
+    for i in k:
+        try:
+            if(not(i in l)):
+                return False
+            if(not(fc1.get[i] == fc2.get[i])):
+                return False    
+        except:
+            pass
+    return True    
+
+
+# v = validAnagram("","") #true
+# v = validAnagram("aaz","zza") #false
+# v = validAnagram("anagram","nagaram") #true
+# v = validAnagram("rat","car") #false
+# v = validAnagram("awesome","awesom") #false
+#v = validAnagram("qwerty","qeywrt") #true 
+#v = validAnagram("texttwisttime","timetwisttext") #true
+
+v = validAnagram("rar","raa")
+print(v)
+
