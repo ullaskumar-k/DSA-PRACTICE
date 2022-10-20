103ae353fc98e8150e4793ffd6a59ace5a1efd14
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-20 10:50:16 +0530

    Refactor with frequency counter method in same.js

--- practice/2_same/same.js
+++ practice/2_same/same.js
@@ -25,17 +25,43 @@ the array has its corresponding value squared in the second array. The frequency
 //         return false;
 // }
 
+
+// 2nd try 
+// function same(a,b){
+//     if(a.length != b.length){
+//         return false;
+//     }
+//     var count = 0;
+//     for(let i=0;i<a.length;i++){
+//         let d = b.indexOf(a[i] ** 2);
+//         if(d === -1){
+//             return false;                  
+//         }
+//         b.splice(d,1);
+//     }
+//     return true;
+// }
+
+
+// Frequency counter method
+
 function same(a,b){
-    if(a.length != b.length){
+    if(a.length !== b.length){
         return false;
-    }
-    var count = 0;
-    for(let i=0;i<a.length;i++){
-        let d = b.indexOf(a[i] ** 2);
-        if(d === -1){
-            return false;                  
-        }
-        b.splice(d,1);
+    } 
+    let fc1 = {}
+    let fc2 = {}
+    for(let i of a){
+        fc1[i] = (fc1[i] || 0) + 1;
+    }    
+    for(let i of b){
+        fc2[i] = (fc2[i] || 0) + 1;
+    }    
+    for(let i in fc1){
+        if(!(fc1[i]**2 in fc2))
+            return false;
+        if(!(fc2[i**2] == fc1[i]))
+            return false;    
     }
     return true;
 }
