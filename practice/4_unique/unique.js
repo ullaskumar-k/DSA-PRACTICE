3f33644518f0a169e474b0f8e2b8f02fedf06bd6
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-21 10:40:21 +0530

    Refactore code using Multiple Pointers in unique.js

--- practice/4_unique/unique.js
+++ practice/4_unique/unique.js
@@ -8,28 +8,48 @@
 // countUniqueValues([]) //0
 // countUniqueValues([-2,-1,-1,0,1]) //4
 
+
+// 1st try
+
+// function countUniqueValues(a){
+//    let count = 0;
+//    let i = -1;
+//    let j = 1;
+//    for(let k = 0; k<a.length;k++){
+//      if(k==0){
+//         if(a[k]!=a[j]){
+//            count += 1;
+//         }
+//      }
+//      if(k==a.length-1){
+//         if(a[i]!=a[k]){
+//            count += 1;
+//         }
+//      }
+//      if(k!=0 && k!=a.length-1 && a[i]!=a[k] && a[k]!=a[j]){
+//         count += 1;
+//      }
+//      i += 1;
+//      j += 1;
+//    }
+//    console.log(count);
+//   }
+  
+//   countUniqueValues([-2,-1,-1,0,1]);
+
+
+// 2nd try
 function countUniqueValues(a){
-   let count = 0;
-   let i = -1;
+   let b = [];
    let j = 1;
-   for(let k = 0; k<a.length;k++){
-     if(k==0){
-        if(a[k]!=a[j]){
-           count += 1;
-        }
-     }
-     if(k==a.length-1){
-        if(a[i]!=a[k]){
-           count += 1;
-        }
-     }
-     if(k!=0 && k!=a.length-1 && a[i]!=a[k] && a[k]!=a[j]){
-        count += 1;
-     }
-     i += 1;
-     j += 1;
+   for(let i = 0;i<a.length;i++){
+      if(a[i]!=a[j] || a[i]==a[j]){
+         if(!b.includes(a[i])){
+            b.push(a[i]);
+         }
+      }
    }
-   console.log(count);
-  }
+   console.log(b.length)
+}
   
   countUniqueValues([-2,-1,-1,0,1]);
