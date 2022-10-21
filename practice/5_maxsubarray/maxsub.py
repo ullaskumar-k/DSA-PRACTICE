9831ea100943ad4a1985cd97ba5a703ecf1f3eeb
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-21 16:00:29 +0530

    Refactor using Sliding Window Pattern in maxsub.py

--- practice/5_maxsubarray/maxsub.py
+++ practice/5_maxsubarray/maxsub.py
@@ -7,25 +7,39 @@
 # maxSubarraySum([4,2,1,6,2],4) //13
 # maxSubarraySum([],4) //null
 
+# 1st try
+# def maxSubarraySum(a,k):
+#     if len(a) < k:
+#         return None
+#     sum = 0
+#     temp = 0
+#     for i in range(0,len(a)+1):
+#         temp = 0
+#         try:    
+#             for j in range(0,k):
+#                 temp += a[i+j]
+#             if(temp>sum):
+#                 sum = temp
+#         except:
+#             pass
+#     print(sum) 
 
+#Sliding window pattern
 def maxSubarraySum(a,k):
-    if len(a) < k:
+    if len(a)<k:
         return None
-    sum = 0
-    temp = 0
-    for i in range(0,len(a)+1):
-        temp = 0
-        try:    
-            for j in range(0,k):
-                temp += a[i+j]
-            if(temp>sum):
-                sum = temp
-        except:
-            pass
-    print(sum) 
+    sum = 0   
+    for i in range(0,k+1):
+        sum += a[i]
+    temp = sum 
+    for i in range(0,len(a)-k+1):
+        temp = temp + a[i] - a[i-k]
+        if temp>sum:
+            sum = temp
+    print(sum)                   
 
 # maxSubarraySum([1,2,5,2,8,1,5],2) #10
-maxSubarraySum([1,2,5,2,8,1,5],4) #17
-# maxSubarraySum([4,2,1,6],1) //6
-# maxSubarraySum([4,2,1,6,2],4) //13
-# maxSubarraySum([],4) //null
+# maxSubarraySum([1,2,5,2,8,1,5],4) #17
+maxSubarraySum([4,2,1,6],1) #6
+# maxSubarraySum([4,2,1,6,2],4) #13
+# maxSubarraySum([],4) #null
