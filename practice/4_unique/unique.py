9f65032de8ce71aa77727211c416a873fd7b1154
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-21 10:24:18 +0530

    Completed unique.py

--- practice/4_unique/unique.py
+++ practice/4_unique/unique.py
@@ -6,4 +6,22 @@
 # countUniqueValues([1,1,1,1,1,2]) // 2
 # countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) //7
 # countUniqueValues([]) //0
-# countUniqueValues([-2,-1,-1,0,1]) //4
+# countUniqueValues([-2,-1,-1,0,1]) //3
+def countUniqueValues(a):
+    i = -1
+    j = 1
+    count = 0
+    for k in range(0,len(a)):
+        if k == 0:
+            if(a[k]!=a[j]):
+                count += 1
+        if k == len(a)-1:
+            if(a[i]!=a[k]):
+                count += 1 
+        if k!=0 and k!=len(a)-1 and a[i]!=a[k] and a[k]!=a[j]:
+            count += 1
+        i += 1
+        j += 1
+    print(count)
+
+countUniqueValues([-2,-1,-1,0,1])
