4280e103c925b4a89b20a480366cac0a91ff81e4
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-21 15:27:31 +0530

    Completed maxsub.js

--- practice/5_maxsubarray/maxsub.js
+++ practice/5_maxsubarray/maxsub.js
@@ -7,3 +7,30 @@
 // maxSubarraySum([4,2,1,6,2],4) //13
 // maxSubarraySum([],4) //null
 
+
+function maxSubarraySum(a,k){
+    if(a.length<k){
+        return null;
+    }
+    var temp_sum = 0;
+    var sum = 0;
+    for(let i=0;i<a.length;i++){
+        temp_sum = 0;
+        for(let j=0;j<k;j++){
+            temp_sum = temp_sum + a[i+j];
+        }
+        if(temp_sum>sum){
+            sum = temp_sum;
+        }
+
+    }
+    console.log(sum);   
+
+}
+
+
+// maxSubarraySum([1,2,5,2,8,1,5],2) //10
+// maxSubarraySum([1,2,5,2,8,1,5],4) //17
+// maxSubarraySum([4,2,1,6],1) //6
+// maxSubarraySum([4,2,1,6,2],4) //13
+// maxSubarraySum([],4) //null
