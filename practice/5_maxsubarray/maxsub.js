c2299be368001d5e9fcab911859860d2ed3b5dbe
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-21 16:00:09 +0530

    Refactor using Sliding Window Pattern in maxsub.js

--- practice/5_maxsubarray/maxsub.js
+++ practice/5_maxsubarray/maxsub.js
@@ -7,30 +7,48 @@
 // maxSubarraySum([4,2,1,6,2],4) //13
 // maxSubarraySum([],4) //null
 
+// 1st try
+// function maxSubarraySum(a,k){
+//     if(a.length<k){
+//         return null;
+//     }
+//     var temp_sum = 0;
+//     var sum = 0;
+//     for(let i=0;i<a.length;i++){
+//         temp_sum = 0;
+//         for(let j=0;j<k;j++){
+//             temp_sum = temp_sum + a[i+j];
+//         }
+//         if(temp_sum>sum){
+//             sum = temp_sum;
+//         }
 
+//     }
+//     console.log(sum);   
+
+// }
+
+//Sliding Window Pattern
 function maxSubarraySum(a,k){
     if(a.length<k){
         return null;
     }
-    var temp_sum = 0;
     var sum = 0;
-    for(let i=0;i<a.length;i++){
-        temp_sum = 0;
-        for(let j=0;j<k;j++){
-            temp_sum = temp_sum + a[i+j];
-        }
-        if(temp_sum>sum){
-            sum = temp_sum;
-        }
-
+    for(let i=0;i<k;i++){
+        sum += a[i];
     }
-    console.log(sum);   
-
+    var temp = sum;
+    for(let i=k;i<a.length;i++){ //4216 
+        temp = temp + a[i] - a[i-k];
+        if(temp>sum){
+            sum = temp;
+        }
+    }
+    console.log(sum)
 }
-
 
 // maxSubarraySum([1,2,5,2,8,1,5],2) //10
 // maxSubarraySum([1,2,5,2,8,1,5],4) //17
-// maxSubarraySum([4,2,1,6],1) //6
+maxSubarraySum([4,2,1,6],1) //6
 // maxSubarraySum([4,2,1,6,2],4) //13
 // maxSubarraySum([],4) //null
