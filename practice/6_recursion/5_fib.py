399f80be7528474b89eea5726e08e5a3b515f28e
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-25 18:47:28 +0530

    completed fib.py

---
+++ practice/6_recursion/5_fib.py
@@ -0,0 +1,17 @@
+# Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. 
+# Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ...
+# which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+# fib(4) // 3
+# fib(10) // 55
+# fib(28) // 317811
+# fib(35) // 9227465
+
+def fib(a):
+    if(a==1 or a==2):
+        return 1
+    return fib(a-1)+fib(a-2)    
+
+print(fib(4) ) #// 3
+print(fib(10)) #// 55
+print(fib(28)) #// 317811
+print(fib(35)) #// 9227465    
