56ff6932e7c4361cd85ec197e888c7888457a777
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-25 17:06:01 +0530

    completed power.py

--- practice/6_recursion/1_power.py
+++ practice/6_recursion/1_power.py
@@ -1,3 +1,13 @@
+# Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. 
+# This function should mimic the functionality of Math.pow()  - do not worry about negative bases and exponents.
 # // power(2,0) // 1
 # // power(2,2) // 4
 # // power(2,4) // 16
+import math
+def power(b,e):
+    if e == 0:
+        return print(1)
+    return print(math.pow(b,e))    
+
+power(2,0)
+power(2,2)
