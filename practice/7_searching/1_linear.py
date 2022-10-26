a0f9ee698685427f604ee01a55405654fe0fbb46
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-26 18:18:40 +0530

    complete linear.py

--- practice/7_searching/1_linear.py
+++ practice/7_searching/1_linear.py
@@ -3,3 +3,13 @@
 # ->Loop through the array and check if the current array element is equal to the value
 # ->If it is equal, return the index at which the element is found
 # ->If the value is never found, return -1
+def linear(arr,val):
+    c = 1
+    for i in range(0,len(arr)):
+        if arr[i] == val:
+            c -= 1
+            print(arr.index(val))
+    if c:
+        print(-1)        
+
+linear([1,2,3],5)
