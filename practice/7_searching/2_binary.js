6065f396889c905386a82a71b2b2f77be6dbca53
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-26 20:17:10 +0530

    complete binary.js

--- practice/7_searching/2_binary.js
+++ practice/7_searching/2_binary.js
@@ -6,3 +6,21 @@
 //     ->If value is too small, move left pointer towards right 
 //     ->If value is too innerHeight, move right pointer to left
 // ->If you never find the value, return -1    
+
+function binary(arr,val){
+    var l = 0;  
+    var r = arr.length-1;
+    var mid = Math.floor((l+r)/2);
+    while(arr[mid]!=val && l<=r){
+        if(arr[mid]>val){
+            r = mid-1;
+        }
+        if(arr[mid]<val){
+            l = mid+1;
+        }
+        mid = Math.floor((l+r)/2);
+    }
+    console.log(arr[mid] == val ? mid : -1);
+}
+
+binary([1,2,3,4,5],12);
