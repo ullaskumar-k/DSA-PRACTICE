cac2079aa500affe6e9408627460847e8cf94e4e
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-26 20:17:18 +0530

    complete binary.py

--- practice/7_searching/2_binary.py
+++ practice/7_searching/2_binary.py
@@ -6,3 +6,19 @@
 #     ->If value is too small, move left pointer towards right 
 #     ->If value is too innerHeight, move right pointer to left
 # ->If you never find the value, return -1    
+def binary(arr,val):
+    l = 0
+    r = len(arr)-1
+    mid = int((l+r)/2)
+    while arr[mid]!=val and l <= r:
+        if val>arr[mid]:
+            l = mid + 1
+        else:
+            r = mid - 1
+        mid = int((l+r)/2) 
+    if arr[mid] == val:
+        print(mid)
+    else:
+        print(-1)
+
+binary([1,2,3,4,5],6)                   
