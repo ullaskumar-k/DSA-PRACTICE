7de516e63e96404875e8a5767d79b8609893c156
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-26 18:45:46 +0530

    start binary program

---
+++ practice/7_searching/2_binary.js
@@ -0,0 +1,8 @@
+// ->This function accepts a sorted array and a value 
+// ->Create a left pointer on start of the array, and a right pointer at the end of the array
+// ->While the left pointer comes before the right pointer
+//     ->Create a pointer in the middle 
+//     ->If you find value you want, return the index
+//     ->If value is too small, move left pointer towards right 
+//     ->If value is too innerHeight, move right pointer to left
+// ->If you never find the value, return -1    
