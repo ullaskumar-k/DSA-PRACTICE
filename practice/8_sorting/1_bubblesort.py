b3758474b8e8cb5b2777c5ab06ec69d27a7d2e75
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-27 16:48:54 +0530

    complete bubblesort.py

--- practice/8_sorting/1_bubblesort.py
+++ practice/8_sorting/1_bubblesort.py
@@ -4,3 +4,13 @@
 # ->Start an inner loop with a variable called j from the beginning until i-1
 # ->If arr[j] is greater than arr[j+1], swap those two values
 # ->Return the sorted array
+def bubblesort(a):
+    for i in range(len(a),0,-1):
+        for j in range(0,len(a)-1):
+            if a[j] > a[j+1]:
+                temp = a[j]
+                a[j] = a[j+1]
+                a[j+1] = temp
+    print(a)            
+
+bubblesort([50,60,2,55,3,1])
