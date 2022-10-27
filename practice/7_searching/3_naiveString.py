9c655089d8e59a808aeb6146e25f2408a3e26086
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-27 10:59:37 +0530

    complete naiveString.py

--- practice/7_searching/3_naiveString.py
+++ practice/7_searching/3_naiveString.py
@@ -6,3 +6,63 @@
 # ->If the characters do match, keep going
 # ->If you complete the inner loop and find a match, increment the count of matches
 # ->Return the count
+
+
+# def Naive(str1,str2):
+#     i,d,count,o = 0,0,0,0
+#     for k in range(0,len(str1)):
+#         i,d = k,0 
+#         for j in range(0,len(str2)):
+#             try:
+#                 if(str1[i]==str2[j] and d<len(str2)):
+#                     d += 1
+#             except:
+#                 pass
+#             i += 1
+#             if d == len(str2):
+#                 count += 1
+#     print(count)    
+# Naive("abdabdefgabd","abd")
+
+
+
+# def Naive(str1,str2):
+#     if len(str2)>len(str1): 
+#         print("Enter valid second string")
+#     i,d = 0,0
+#     count = 0
+#     for k in range(0,len(str1)):
+#         i,d = k,0 
+#         for j in range(0,len(str2)):
+#             # print(f"{i} == {j}")
+#             if(str1[i]==str2[j] and d<=len(str2)):
+#                 d += 1
+#             if d == len(str2):
+#                 count += 1
+#             i += 1
+#             if i == len(str1):
+#                 return print(count)
+# Naive("abdefggabdabdabd","abd")
+
+
+def Naive(str1,str2):
+    if len(str2)>len(str1): 
+        print("Enter valid second string")
+    count,i,j,k,d = 0,0,0,0,0
+    while(k!=len(str1)):
+        i = k
+        d,j = 0,0
+        while(j!=len(str1)):
+            if str1[i] == str2[j] and d<len(str2):
+                d += 1
+            if d == len(str2):
+                count += 1
+            i += 1
+            j += 1    
+        if k == len(str1):
+            return print(count)
+        k+=1              
+
+Naive("abdefggabdabdabd","abd")
+
+
