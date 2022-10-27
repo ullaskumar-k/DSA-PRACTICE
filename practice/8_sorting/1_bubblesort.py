28a9fb74fa49c3a6755ed5c902c33eadb758c52a
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-27 16:27:52 +0530

    start bubble sort program

---
+++ practice/8_sorting/1_bubblesort.py
@@ -0,0 +1,6 @@
+# PSEUDOCODE
+# ->Function that accepts a array and that are numbers
+# ->Start looping from with a variable called i from the end of the array to the beginning
+# ->Start an inner loop with a variable called j from the beginning until i-1
+# ->If arr[j] is greater than arr[j+1], swap those two values
+# ->Return the sorted array
