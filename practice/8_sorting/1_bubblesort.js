022bcc11abe6008602659121f53357fdda05e4ec
ULLASKUMAR K <ullaskumar2k@gmail.com>
2022-10-27 16:48:47 +0530

    complete bubblesort.js

--- practice/8_sorting/1_bubblesort.js
+++ practice/8_sorting/1_bubblesort.js
@@ -4,3 +4,17 @@
 // ->Start an inner loop with a variable called j from the beginning until i-1
 // ->If arr[j] is greater than arr[j+1], swap those two values
 // ->Return the sorted array
+function bubblesort(arr){
+    for(let i=arr.length; i>=0;i--){
+        for(let j = 0;j<arr.length;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    console.log(arr);
+}
+
+bubblesort([50,2,6,52,1]);
